e73efe9c406b8881654d3bbe2105ee6a28c3c9ba
Elidji <rock89b@gmail.com>
2019-04-01 19:49:50 +0200

    Extraction Zone Polish (ajout du decal et création du BP)

--- Source/FPSGame/Private/FPSExtractionZone.cpp
+++ Source/FPSGame/Private/FPSExtractionZone.cpp
@@ -2,6 +2,7 @@
 
 #include "FPSExtractionZone.h"
 #include "Components/BoxComponent.h"
+#include "Components/DecalComponent.h"
 
 // Sets default values
 AFPSExtractionZone::AFPSExtractionZone()
@@ -10,7 +11,10 @@ AFPSExtractionZone::AFPSExtractionZone()
 	OverlapComp->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
 	OverlapComp->SetCollisionResponseToAllChannels(ECR_Ignore);
 	OverlapComp->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
-	OverlapComp->SetBoxExtent(FVector(200.0f));
+
+	// Définie la taille de la box, si FVector n'a qu'un paramètre les 3 coordonnées ont cette valeurs = (200.f, 200.f, 200.f)
+	OverlapComp->SetBoxExtent(FVector(200.f));
+
 	RootComponent = OverlapComp;
 
 	// Rend visible la box OverlapComp
@@ -18,6 +22,10 @@ AFPSExtractionZone::AFPSExtractionZone()
 
 	// Permet de lier l'événement OnComponentBeginOverlap à la méthode HandleOverlap
 	OverlapComp->OnComponentBeginOverlap.AddDynamic(this, &AFPSExtractionZone::HandleOverlap);
+
+	DecalComp = CreateAbstractDefaultSubobject<UDecalComponent>(TEXT("DecalComp"));
+	DecalComp->DecalSize = FVector(200.f);
+	DecalComp->SetupAttachment(RootComponent);
 }
 
 
--- Source/FPSGame/Public/FPSExtractionZone.h
+++ Source/FPSGame/Public/FPSExtractionZone.h
@@ -21,6 +21,9 @@ protected:
 	UPROPERTY(VisibleAnywhere, Category = "Components")
 	UBoxComponent* OverlapComp;
 
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UDecalComponent* DecalComp;
+
 	UFUNCTION()
 	void HandleOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 };
